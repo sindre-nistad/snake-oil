7fa00d8a0ff09310f38de2d86adf7ba61c900e8a
Sindre Nistad <38072308+sindre-nistad@users.noreply.github.com>
2025-06-19 09:28:11 +0200

    feat: Add cython implementation
    Currently, it works seems quite inefficient
    Potentially it could be shown as a suggestion / somewhere to start

---
+++ src/mandelbrot/implementations/cython/cython_naive/README.md
@@ -0,0 +1,4 @@
+# Cython implementation of Mandelbrot
+
+This implementation is very close to a direct port from the pure Python implementation.
+While fairly straight forward to make, the annotated source code reveal many interactions with the Python runtime.
---
+++ src/mandelbrot/implementations/cython/cython_naive/_compute.pyx
@@ -0,0 +1,80 @@
+# cython: infer_types=True
+# distutils: language=c
+# cython: boundscheck=False
+# cython: cdivision=True
+# cython: exceptval=-1
+
+import cython
+from cython import double, int, uint
+from cython.parallel import parallel, prange
+import cython.cimports.numpy as cnp
+import numpy as np
+
+
+cnp.import_array()
+
+
+@cython.cfunc
+@cython.nogil
+def mandelbrot(x: double, y: double, cutoff: uint) -> uint:
+    """Compute the margins of the mandelbrot set"""
+    z = 0 + 0j
+    c = x + y * 1j
+    iterations = cython.declare(uint, 0)
+    while iterations < cutoff and abs(z) <= 2:
+        z = z**2 + c
+        iterations += 1
+    # The first iteration could be considered the zeroth, as z will always be 0
+    # in that iteration, so the loop will be executed at least once.
+    return iterations - 1
+
+
+@cython.locals(
+    x_scale=double,
+    y_scale=double,
+    i=int,
+    j=int,
+    x_min=double,
+    x_max=double,
+    y_min=double,
+    y_max=double,
+)
+@cython.ccall
+def compute_mandelbrot(
+    width: int,
+    height: int,
+    x: tuple[double, double],
+    y: tuple[double, double],
+    cutoff: uint,
+):
+    divergence: uint[:, ::1] = np.zeros((width, height), dtype=np.uint32)
+
+    x_min, x_max = x
+    y_min, y_max = y
+
+    x_scale = abs(x_min - x_max) / width
+    y_scale = abs(y_min - y_max) / height
+
+    with cython.nogil(), parallel():
+        for i in prange(width):
+            for j in prange(height):
+                divergence[i, j] = mandelbrot(
+                    x_min + i * x_scale, y_min + j * y_scale, cutoff
+                )
+    return divergence
+
+
+@cython.locals(
+    n=int,
+    m=int,
+    i=int,
+    j=int,
+)
+def apply_colormap(divergence: uint[:, ::1], cutoff: uint, colormap):
+    conv = np.floor(np.asarray(divergence) / cutoff * len(colormap)).astype(np.uint64)
+    n, m = conv.shape
+    pixels = np.zeros((n, m, 3), dtype=np.uint8)
+    for i in range(n):
+        for j in range(m):
+            pixels[i, j, :] = colormap[conv[i][j]]
+    return pixels
