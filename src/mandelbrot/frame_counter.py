fc9e551b6e9eb68e358b1a6e3273b4ff562dc9f6
Sindre Nistad <38072308+sindre-nistad@users.noreply.github.com>
2025-06-29 01:51:51 +0200

    feat: Use the screen's complimentary color for frame counter
    That way, it weill be readable even if the screen is white / bright at the top of the corner.
    
    I've also reduced the size of the info box to be slightly bigger than the amount of space the text takes up.
    
    This was found by adding a text box around the info box
    
    ```python
    pygame.draw.lines(self.screen, (255, 255, 255), True, [(0, 0), (self.info.width, 0), (self.info.width, self.info.height), (0, self.info.height)])
    ```

--- src/mandelbrot/frame_counter.py
+++ src/mandelbrot/frame_counter.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import pygame
 
 
@@ -7,7 +8,7 @@ class FrameCounter:
     def __init__(self, screen: pygame.Surface):
         self.screen = screen
         self.clock = pygame.time.Clock()
-        self.info = pygame.rect.Rect(0, 0, 400, 100)
+        self.info = pygame.rect.Rect(0, 0, 100, 30)
         self.font = pygame.font.Font(
             Path(__file__).parent
             / "assets"
@@ -16,13 +17,16 @@ class FrameCounter:
             / "LiberationSans.ttc",
             24,
         )
+        self._num_pixes = self.info.width * self.info.height
 
     def update(self):
+        colors = pygame.surfarray.pixels3d(self.screen.subsurface(self.info).copy())
+        average_color = (colors.sum(axis=(0, 1)) / self._num_pixes).astype(np.uint8)
+        color = np.array([255, 255, 255], dtype=np.uint8) - average_color
+
         dt = self.clock.tick(60) / 1000
         self.screen.blit(
-            self.font.render(
-                f"{1 / dt:.2f} fps" if dt < 1 else f"{dt} spf", 1, (255, 255, 255)
-            ),
+            self.font.render(f"{1 / dt:.2f} fps" if dt < 1 else f"{dt} spf", 1, color),
             self.info,
         )
         pygame.display.update()
