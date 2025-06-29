e32bd9acdf65f9bae876c051ede69f5d3e54982f
Sindre Nistad <38072308+sindre-nistad@users.noreply.github.com>
2025-06-29 19:33:02 +0200

    docs: Add some notes on Rust
    (how to parallelize it)

--- src/presentation/main.js
+++ src/presentation/main.js
@@ -2,12 +2,13 @@ import Reveal from "reveal.js";
 import Highlight from "reveal.js/plugin/highlight/highlight.esm";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm";
 import RevealMath from "reveal.js/plugin/math/math.esm";
+import Notes from "reveal.js/plugin/notes/notes.esm";
 import "./assets/atelier-dune-light.css";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/sky.css";
 
 Reveal.initialize({
-	plugins: [Markdown, RevealMath.KaTeX, Highlight],
+	plugins: [Markdown, RevealMath.KaTeX, Notes, Highlight],
 	hash: true,
 	progress: true,
 	markdown: {
